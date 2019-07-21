0f10443b06d0eb953756ec25270455341412a445
Rafael <ffael@live.com>
2019-07-21 13:04:04 -0400

    Added Twitter Bootstrap CSS

--- src/components/Layouts/head.js
+++ src/components/Layouts/head.js
@@ -12,7 +12,12 @@ const Head = ({title}) =>{
             }
         }
         `)
-    return <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
+    return (
+        <Helmet>
+            <title>{`${title} | ${data.site.siteMetadata.title}`}</title>
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+        </Helmet>
+    )
 }
 
 export default Head
