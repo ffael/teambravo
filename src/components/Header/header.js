a638d2fcc4720d799215d824c89cd79f9ea8d734
Rafael <ffael@live.com>
2019-07-25 11:03:24 -0400

    Added Header and Layout styles

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -18,28 +18,22 @@ const Header = () =>{
     `)
 
     return(
-        <header className={HeaderStyles.header}>
-            <section className={`${HeaderStyles.headerContent} content`}>
-                <h1><img src={Logo} alt={data.site.siteMetadata.title}></img></h1>
-                <nav className={HeaderStyles.mainNav}>
-                    <ul>
-                        <li><Link to="/" activeClassName={HeaderStyles.activeNav}>Home</Link></li>
-                        <li><Link to="/services" activeClassName={HeaderStyles.activeNav}>Services</Link></li>
-                        <li><Link to="/about" activeClassName={HeaderStyles.activeNav}>About</Link></li>
-                        <li><Link to="/contact" activeClassName={HeaderStyles.activeNav}>Contact</Link></li>
-                    </ul>
-                </nav>
-                <Link to="/" className={HeaderStyles.requestBtn}>Request Estimate</Link>
-            </section>
-
-            <section className={HeaderStyles.hero}>
-                <div className={`${HeaderStyles.content} content`}>
-                    <h1>Hello</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                </div>
-            </section>
-            
-        </header>
+        <div className={HeaderStyles.headerWrapper}>
+            <header className={HeaderStyles.header}>
+                <section className={`${HeaderStyles.headerContent} content`}>
+                    <h1><img src={Logo} alt={data.site.siteMetadata.title}></img></h1>
+                    <nav className={HeaderStyles.mainNav}>
+                        <ul>
+                            <li><Link to="/" activeClassName={HeaderStyles.activeNav}>Home</Link></li>
+                            <li><Link to="/services" activeClassName={HeaderStyles.activeNav}>Services</Link></li>
+                            <li><Link to="/about" activeClassName={HeaderStyles.activeNav}>About</Link></li>
+                            <li><Link to="/contact" activeClassName={HeaderStyles.activeNav}>Contact</Link></li>
+                        </ul>
+                    </nav>
+                    <Link to="/" className={HeaderStyles.requestBtn}>Request Estimate</Link>
+                </section>            
+            </header>
+        </div>
     )
 }
 
--- src/components/Layouts/layout.js
+++ src/components/Layouts/layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Header/header.js'
+import Hero from '../Hero/hero.js'
 import Footer from '../Footer/footer.js'
 import LayoutStyles from './styles.module.scss'
 
@@ -11,6 +12,7 @@ const Layout = ({children}) => {
         <div className={LayoutStyles.wrapper}>
             <Header />
             <main className={LayoutStyles.content}>
+            <Hero />
                 {children}
             </main>
             <Footer />
