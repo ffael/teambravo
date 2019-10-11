c8a8df375786c8b0c961d9cc33a2f1c4be6cf969
Rafael <ffael@live.com>
2019-10-11 12:05:21 -0400

    Added Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,16 +52,22 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        path: `${__dirname}/src/assets/img`
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/services/img`
-      }
+        name: "Team Bravo",
+        short_name: "Bravo",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#570A0A",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/assets/img/favicon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
     },
   ]
 }
